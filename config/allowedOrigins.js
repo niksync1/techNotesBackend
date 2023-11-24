5deaac96ac48d30094e84ce9ec1412da16cdbfcd
niksync1 <40718182+niksync1@users.noreply.github.com>
2023-11-24 20:18:28 +0000

    Update allowedOrigins.js to allow hosted site on render

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,7 @@
 
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://technotes-7n7s.onrender.com',
-    
+    'https://technotes-7n7s.onrender.com'    
 ]
 
 module.exports = allowedOrigins
