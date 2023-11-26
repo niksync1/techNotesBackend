529280767374944236c2499d38b1f71206d93803
niksync1 <40718182+niksync1@users.noreply.github.com>
2023-11-26 18:02:43 +0000

    added category option to notesController

--- models/Note.js
+++ models/Note.js
@@ -16,6 +16,10 @@ const noteSchema = new mongoose.Schema(
             type: String,
             required: true
         },
+        category: {
+            type: String,
+            required: true
+        },
         completed: {
             type: Boolean,
             default: false
